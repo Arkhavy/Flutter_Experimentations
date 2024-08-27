f765cc5de26a844fb05f3a9fd7d4f48fc59ff539
arkstrix <arkhavy@gmail.com>
2024-08-27 17:29:28 +0200

    Added a third correct color wheel, things are starting to look good :D

--- Code/playground/lib/main.dart
+++ Code/playground/lib/main.dart
@@ -1,47 +1,47 @@
 import 'package:flutter/material.dart';
 
 int rotation = 0;
-int red = 0;
-int green = 0;
-int blue = 0;
+int r = 0;
+int g = 0;
+int b = 0;
 
 int monochrome = 0;
 bool direction = true;
 
 // Just some experimentation, clean code is out of the window anyway for scientific purpose :D
 void  incrementrot0() {
-  if (red < 255) {
-    red++;
+  if (r < 255) {
+    r++;
   }
-  else if (red == 255 && green < 255) {
-    green++;
+  else if (r == 255 && g < 255) {
+    g++;
   }
-  else if (red == 255 && green == 255 && blue < 255) {
-    blue++;
+  else if (r == 255 && g == 255 && b < 255) {
+    b++;
   }
 }
 
 void  incrementrot1() {
-  if (green < 255) {
-    green++;
+  if (g < 255) {
+    g++;
   }
-  else if (green == 255 && blue < 255) {
-    blue++;
+  else if (g == 255 && b < 255) {
+    b++;
   }
-  else if (green == 255 && blue == 255 && red < 255) {
-    red++;
+  else if (g == 255 && b == 255 && r < 255) {
+    r++;
   }
 }
 
 void  incrementrot2() {
-  if (blue < 255) {
-    blue++;
+  if (b < 255) {
+    b++;
   }
-  else if (blue == 255 && red < 255) {
-    red++;
+  else if (b == 255 && r < 255) {
+    r++;
   }
-  else if (blue == 255 && red == 255 && green < 255) {
-    green++;
+  else if (b == 255 && r == 255 && g < 255) {
+    g++;
   }
 }
 
@@ -54,14 +54,59 @@ void  rotationHub() {
     case 2:
       incrementrot2(); break;
   }
-  if (red == 255 && green == 255 && blue == 255) {
-    red = 0;
-    green = 0;
-    blue = 0;
+  if (r == 255 && g == 255 && b == 255) {
+    r = 0;
+    g = 0;
+    b = 0;
     rotation++;
     if (rotation == 3) {
       rotation = 0;
     }
+  }
+}
+
+int red = 255;
+int green = 0;
+int blue = 0;
+int state = 0;
+void  colorWheel() {
+  switch (state) {
+    case 0: // increment blue
+      blue++;
+      if (blue == 255) {
+        state++;
+      }
+      break;
+    case 1: // decrement red
+      red--;
+      if (red == 0) {
+        state++;
+      }
+      break;
+    case 2: // increment green
+      green++;
+      if (green == 255) {
+        state++;
+      }
+      break;
+    case 3: // decrement blue
+      blue--;
+      if (blue == 0) {
+        state++;
+      }
+      break;
+    case 4: // increment red
+      red++;
+      if (red == 255) {
+        state++;
+      }
+      break;
+    case 5: // decrement green
+      green--;
+      if (green == 0) {
+        state = 0;
+      }
+      break;
   }
 }
 
@@ -90,7 +135,7 @@ class StartingPoint extends StatelessWidget {
                 rotationHub();
                 // print("red = $red | green = $green | blue = $blue");
                 return Container(
-                  color: Color.fromARGB(255, red, green, blue),
+                  color: Color.fromARGB(255, r, g, b),
                   width: 500,
                   height: 5,
                 );
@@ -112,6 +157,19 @@ class StartingPoint extends StatelessWidget {
                   }
                   return Container(
                     color: Color.fromARGB(255, monochrome, monochrome, monochrome),
+                    width: 500,
+                    height: 5,
+                  );
+                }, // itemBuilder
+              ),
+            ),
+            Flexible(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  colorWheel();
+                  return Container(
+                    color: Color.fromARGB(255, red, green, blue),
                     width: 500,
                     height: 5,
                   );
