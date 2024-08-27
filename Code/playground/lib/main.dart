b490e5ce80690f7bfdbbb1da40544bf85de78a78
arkstrix <arkhavy@gmail.com>
2024-08-27 17:17:26 +0200

    Managed to fit two ListView.builder side to side to create color wheels, starting to get a grasp of some concepts with Flutter :D Added a stock.dart file to append weird experiments somewhere else

--- Code/playground/lib/main.dart
+++ Code/playground/lib/main.dart
@@ -5,6 +5,10 @@ int red = 0;
 int green = 0;
 int blue = 0;
 
+int monochrome = 0;
+bool direction = true;
+
+// Just some experimentation, clean code is out of the window anyway for scientific purpose :D
 void  incrementrot0() {
   if (red < 255) {
     red++;
@@ -76,104 +80,47 @@ class StartingPoint extends StatelessWidget {
           backgroundColor: Colors.amberAccent,
           title: const Text("ouaf ouaf"),
         ),
-        body: ListView.builder(
-          scrollDirection: Axis.vertical,
-          addAutomaticKeepAlives: false,
-          itemBuilder: (_, index) {
-          rotationHub();
-          print("red = $red | green = $green | blue = $blue");
-          return Container(
-            color: Color.fromARGB(255, red, green, blue),
-            width: 500,
-            height: 20,
-          );
-        }),
-        // body: ListView(
-        //   scrollDirection: Axis.vertical,
-        //   addAutomaticKeepAlives: false,
-        //   children: [
-        //     Container(
-        //       color: Colors.blue,
-        //       width: 500,
-        //       height: 500,
-        //     ),
-        //     Container(
-        //       color: Colors.green,
-        //       width: 500,
-        //       height: 500,
-        //     ),
-        //     Container(
-        //       color: Colors.yellow,
-        //       width: 500,
-        //       height: 500,
-        //     ),
-        //   ],
-        // )
-        // body: Center(
-        //   child: Container(
-        //     margin: const EdgeInsets.all(50),
-        //     padding: const EdgeInsets.all(10),
-        //     color: Colors.blue,
-        //     height: 100,
-        //     width: 100,
-        //     child: const Text("woof woof"),
-        //   ),
-        // ),
-        // body: const Row(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   crossAxisAlignment: CrossAxisAlignment.end,
-        //   children: [
-        //     Icon(Icons.backpack),
-        //     Icon(Icons.leaderboard),
-        //     Icon(Icons.person),
-        //   ],
-        // ),
-        // body: Stack(
-        //   children: [
-        //     Container(
-        //       color: Colors.greenAccent,
-        //       width: 200,
-        //       height: 200,
-        //       child: const Icon(Icons.share),
-        //     ),
-        //     const Positioned(
-        //       top: 50,
-        //       left: 50,
-        //       child: Icon(Icons.verified),
-        //     ),
-        //     const Align(
-        //       alignment: Alignment.center,
-        //       child: Icon(Icons.balance),
-        //     ),
-        //     const Icon(Icons.anchor),
-        //   ],
-        // ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            print('Floating Action Button Pressed woof');
-          }
+        body: Row(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                addAutomaticKeepAlives: false,
+                itemBuilder: (_, index) {
+                rotationHub();
+                // print("red = $red | green = $green | blue = $blue");
+                return Container(
+                  color: Color.fromARGB(255, red, green, blue),
+                  width: 500,
+                  height: 5,
+                );
+              }),
+            ),
+            Flexible(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  if (direction == true) {
+                    monochrome++;
+                  } else {
+                    monochrome--;
+                  }
+                  if (monochrome == 255) {
+                    direction = false;
+                  } else if (monochrome == 0) {
+                    direction = true;
+                  }
+                  return Container(
+                    color: Color.fromARGB(255, monochrome, monochrome, monochrome),
+                    width: 500,
+                    height: 5,
+                  );
+                }, // itemBuilder
+              ),
+            ),
+          ] // children
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'WoofHome',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'WoofBusiness',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'WoofSchool',
-            ),
-          ],
-        ),
-        // drawer: const Drawer(
-        //   child: Text('woofDrawer'),
-        // ),
-      ),
+      )
     );
-  }
+  } // Widget build
 }
---
+++ Code/playground/lib/stock.dart
@@ -0,0 +1,98 @@
+ // body: ListView(
+        //   scrollDirection: Axis.vertical,
+        //   addAutomaticKeepAlives: false,
+        //   children: [
+        //     Container(
+        //       color: Colors.blue,
+        //       width: 500,
+        //       height: 500,
+        //     ),
+        //     Container(
+        //       color: Colors.green,
+        //       width: 500,
+        //       height: 500,
+        //     ),
+        //     Container(
+        //       color: Colors.yellow,
+        //       width: 500,
+        //       height: 500,
+        //     ),
+        //   ],
+        // )
+        // body: Center(
+        //   child: Container(
+        //     margin: const EdgeInsets.all(50),
+        //     padding: const EdgeInsets.all(10),
+        //     color: Colors.blue,
+        //     height: 100,
+        //     width: 100,
+        //     child: const Text("woof woof"),
+        //   ),
+        // ),
+        // body: const Row(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   crossAxisAlignment: CrossAxisAlignment.end,
+        //   children: [
+        //     Icon(Icons.backpack),
+        //     Icon(Icons.leaderboard),
+        //     Icon(Icons.person),
+        //   ],
+        // ),
+        // body: Stack(
+        //   children: [
+        //     Container(
+        //       color: Colors.greenAccent,
+        //       width: 200,
+        //       height: 200,
+        //       child: const Icon(Icons.share),
+        //     ),
+        //     const Positioned(
+        //       top: 50,
+        //       left: 50,
+        //       child: Icon(Icons.verified),
+        //     ),
+        //     const Align(
+        //       alignment: Alignment.center,
+        //       child: Icon(Icons.balance),
+        //     ),
+        //     const Icon(Icons.anchor),
+        //   ],
+        // ),
+        // floatingActionButton: FloatingActionButton(
+        //   child: Icon(Icons.add),
+        //   onPressed: () {
+        //     print('Floating Action Button Pressed woof');
+        //   }
+        // ),
+        // bottomNavigationBar: BottomNavigationBar(
+        //   items: const [
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.home),
+        //       label: 'WoofHome',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.business),
+        //       label: 'WoofBusiness',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.school),
+        //       label: 'WoofSchool',
+        //     ),
+        //   ],
+        // ),
+        // drawer: const Drawer(
+        //   child: Text('woofDrawer'),
+        // ),
+
+                // body: ListView.builder(
+        //   scrollDirection: Axis.vertical,
+        //   addAutomaticKeepAlives: false,
+        //   itemBuilder: (_, index) {
+        //   rotationHub();
+        //   print("red = $red | green = $green | blue = $blue");
+        //   return Container(
+        //     color: Color.fromARGB(255, red, green, blue),
+        //     width: 500,
+        //     height: 20,
+        //   );
+        // }),
